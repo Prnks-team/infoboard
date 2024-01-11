cda42535ac66b87c7017acc81ad28b2098c95782
prnk1243 <151041466+prnk1243@users.noreply.github.com>
2024-01-11 10:42:49 -0500

    Update Funny.js

--- Funny.js
+++ Funny.js
@@ -1,2 +1,6 @@
-console.warn('SKIBIDI TOILET!!!')
-alert('HELP')
+if (confirm('This blog is only open for PC, mac, and ipad users. If you are any of these please press ok. If not press cancel. ')){
+   console.log("user on pc");
+}else{
+   alert("Redirecting off the site.")
+  location.href = 'http://www.google.com';
+}
