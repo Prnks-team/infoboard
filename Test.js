969b006af80e1e9443363fa7ac3bb7213c35cbac
prnk1243 <151041466+prnk1243@users.noreply.github.com>
2024-01-18 10:31:41 -0500

    Delete Test.js
    Signed-off-by: prnk1243 <151041466+prnk1243@users.noreply.github.com>

--- Test.js
+++
@@ -1,8 +0,0 @@
-console.stdlog = console.log.bind(console);
-    console.logs = ["test"];
-    console.log = function(){
-    console.logs.push(Array.from(arguments));
-    console.stdlog.apply(console, arguments);
-    }
-    if (console.logs = ["test"]);
-      console.warn("Console works");
